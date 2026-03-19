217f97d702c56d1cba70898a155abbeb628e74d1
SINOLTD\07799 <n038087427@gmail.com>
2026-03-19 17:02:48 +0800

    [XrResource] 刪除settings內BASE_DIR的修改

--- CoDevStudio/settings.py
+++ CoDevStudio/settings.py
@@ -162,7 +162,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / "templates",
-            BASE_DIR / "XrResource" / "templates",
         ],
         'APP_DIRS': True,
         'OPTIONS': {
